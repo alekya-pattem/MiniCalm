d3f864d31754e9678dc2c1aa3bf08e6295bafd55
Alekya <alekhya@krify.com>
2026-09-16 16:38:40 +0530

    Initial project setup

---
+++ MiniCalm/MiniCalm/MiniCalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCalmApp.swift
+//  MiniCalm
+//
+//  Created by swathipriya pattem on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MiniCalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
